464dca343a5ddea20ae287f2bff260ab148a4734
Daniel Hart <86760228+dani3ljh@users.noreply.github.com>
2024-05-21 12:46:46 -0400

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages the game
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before any of the Update methods are called.
+    /// </summary>
+    private void Start() {
+        Debug.Log("Hello World");
+    }
+
+    /// <summary>
+    /// Update is called every fram, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update() {
+        
+    }
+}
